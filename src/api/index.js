d46f826aa9b9b45655fb21e0352e387d9154a8c3
clroot <abcdkh1209@gmail.com>
2020-06-14 18:50:15 +0900

    auth 라우터 구현

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,11 @@
 import Router from 'koa-router';
 
 import article from './article';
+import auth from './auth';
 
 const api = new Router();
 
 api.use('/articles', article.routes());
+api.use('/auth', auth.routes());
 
 export default api;
