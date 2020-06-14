7eda87e286bc5bd41bebcb471b421d9b46800000
clroot <abcdkh1209@gmail.com>
2020-06-14 19:11:37 +0900

    로그아웃 코드 작성

--- src/api/auth/auth.ctrl.js
+++ src/api/auth/auth.ctrl.js
@@ -71,4 +71,7 @@ export const check = async (ctx) => {
 
   ctx.body = user;
 };
-export const logout = async (ctx) => {};
+export const logout = async (ctx) => {
+  ctx.cookies.set('access_token');
+  ctx.status = 204;
+};
