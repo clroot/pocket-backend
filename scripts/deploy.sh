da02e0d640fa598ef0e57957885cc1518364b183
clroot <abcdkh1209@gmail.com>
2020-09-12 23:43:51 +0900

    deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,12 +6,12 @@ EXIST_BLUE=$(docker ps | grep ${DOCKER_APP_NAME}-blue)
 
 if [ -z "$EXIST_BLUE" ]; then
 	echo "blue is available"
-	docker run -it --name ${DOCKER_APP_NAME}-blue -v $(pwd):/deploy/node-app -p 4001:4000 --network=clroot node-app 
+	docker run -it --name ${DOCKER_APP_NAME}-blue -v $(pwd):/deploy/node-app -p 4001:4000 --network=clroot clroot/node-app:1 
 	sleep 10
 	docker rm -f ${DOCKER_APP_NAME}-green
 else
 	echo "green is available"
-	docker run -it --name ${DOCKER_APP_NAME}-green -v $(pwd):/deploy/node-app -p 4002:4000 --network=clroot node-app 
+	docker run -it --name ${DOCKER_APP_NAME}-green -v $(pwd):/deploy/node-app -p 4002:4000 --network=clroot clroot/node-app:1 
 	sleep 10
 	docker rm -f ${DOCKER_APP_NAME}-blue
 fi
