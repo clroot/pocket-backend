e3eb81cb9e88f297c5e6614e60dedad0e8969f54
clroot <abcdkh1209@gmail.com>
2020-10-08 19:12:36 +0900

    EmailAuth 모델 테스트 작성

---
+++ src/__test__/models/emailAuth.test.js
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+import EmailAuth from '../../models/emailAuth';
+import { generateObjectId } from '../test-utils';
+
+describe('EmailAuth 모델은 ', () => {
+  const sampleToken = '';
+  const sampleUserId = generateObjectId();
+  it('user, token 필드를 가진다.', (done) => {
+    const record = new EmailAuth({ token: sampleToken, user: sampleUserId });
+    expect(record.token).be.eq(sampleToken);
+    expect(record.user).be.eq(sampleUserId);
+    done();
+  });
+});
