d6a34a161f959b0cdb50ad8453a27c9254ea5cf1
clroot <abcdkh1209@gmail.com>
2021-11-23 01:04:53 +0900

    node_modules docker 컨테이너 밖으로 분리

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,6 @@ FROM node:lts
 
 WORKDIR /app/pocket-backend
 
-COPY package*.json ./
-RUN npm install
-
 EXPOSE 4000
-
-COPY . .
 
 CMD ["npm", "start"]
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,12 @@ BACK_END_REPO=pocket-backend
 FRONT_END_REPO=pocket-frontend
 DOCKER_APP_NAME=pocket-backend
 
+EXIST_NODE_MODULES=$(ls -al | grep node_modules)
+if [ -z "$EXIST_NODE_MODULES" ]; then
+  echo "Installing node modules..."
+  npm install
+fi
+
 EXIST_BLUE=$(docker ps -a | grep ${DOCKER_APP_NAME}-blue)
 
 if [ -z "$EXIST_BLUE" ]; then
@@ -20,6 +26,7 @@ docker build ${HOST_DIR}/${BACK_END_REPO} -t ${DOCKER_APP_NAME}
 
 echo "$RUN_TARGET is available"
 docker run -d --name ${DOCKER_APP_NAME}-${RUN_TARGET} \
+  -v $HOST_DIR/$BACK_END_REPO:/app/pocket-backend \
   -v $HOST_DIR/$FRONT_END_REPO/build:/app/pocket-frontend/build \
   -p $PORT:4000 \
   ${DOCKER_APP_NAME}
